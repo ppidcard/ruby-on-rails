c1e52cdbb52f52f9ff76ffb99eb4b8a69aa953e2
ppidcard <tvxqlikei@live.cn>
2021-09-26 17:14:30 +1000

    Adding home page

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
-# Load the Rails application.
-require_relative "application"
+const { environment } = require('@rails/webpacker')
 
-# Initialize the Rails application.
-Rails.application.initialize!
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+module.exports = environment
