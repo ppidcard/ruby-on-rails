ae95edbd79c178263df7fba9ef7dc9eb89578400
ppidcard <tvxqlikei@live.cn>
2021-09-27 08:29:03 +1000

    Adding show page

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,10 +1,9 @@
 class PropertiesController < ApplicationController
-    before_save{ self.address = self.address.downcase }
-    validates :address, presence: true, 
-                uniqueness: {case_sensitive: false}, 
-                length: { minimum: 5, maximum: 25 }
-    validates :photo, presence: true, 
-                uniqueness: {case_sensitive: false}
-    validates :description, presence: true,
-                length: { minimum: 10, maximum: 1000 }
+    def show
+        @property = Property.find(params[:id])
+    end
+
+    def index
+        @properties = Property.all
+    end
 end
