b240649bdd3f43e6dc3a0deeae8e8f4e759d666f
ppidcard <tvxqlikei@live.cn>
2021-09-27 07:51:48 +1000

    Adding validations

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,10 @@
 class PropertiesController < ApplicationController
-
+    before_save{ self.address = self.address.downcase }
+    validates :address, presence: true, 
+                uniqueness: {case_sensitive: false}, 
+                length: { minimum: 5, maximum: 25 }
+    validates :photo, presence: true, 
+                uniqueness: {case_sensitive: false}
+    validates :description, presence: true,
+                length: { minimum: 10, maximum: 1000 }
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,11 @@
 class Property < ApplicationRecord
-
+    before_save{ self.address = self.address.downcase }
+    validates :address, presence: true, 
+                uniqueness: {case_sensitive: false}, 
+                length: { minimum: 5, maximum: 25 }
+    validates :photo, presence: true, 
+                uniqueness: {case_sensitive: false}
+    validates :description, presence: true,
+                length: { minimum: 10, maximum: 1000 }
 
 end
