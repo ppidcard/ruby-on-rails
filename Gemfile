4a736231fb9a871416f87545514afce9a5f95645
ppidcard <tvxqlikei@live.cn>
2021-09-28 08:25:53 +1000

    Adding secure password

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 gem 'sqlite3', '~> 1.4'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
+gem 'bcrypt', '~> 3.1.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 gem 'will_paginate', '~> 3.1.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_221349) do
+ActiveRecord::Schema.define(version: 2021_09_27_222337) do
 
   create_table "properties", force: :cascade do |t|
     t.string "photo"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_221349) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
