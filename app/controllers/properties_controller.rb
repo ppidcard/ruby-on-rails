ab064ac2e29237bc0a5f3eb80cfae89954c2f254
ppidcard <tvxqlikei@live.cn>
2021-09-27 14:28:21 +1000

    Done

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,4 +6,22 @@ class PropertiesController < ApplicationController
     def index
         @properties = Property.paginate(page: params[:page], per_page: 5)    
     end
+
+    def new
+        @property = Property.new
+    end
+
+    def create
+        @property = Property.new(property_params)
+        if @property.save
+            flash[:notice] = "Property was created successfully."
+            redirect_to @property
+          else
+            render 'new'
+          end
+    end
+
+    def property_params
+        params.require(:property).permit(:photo, :address, :description)  
+    end
 end
