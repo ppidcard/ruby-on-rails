00546b3cbf0772f88992d21898cd4c8a019f03ce
ppidcard <tvxqlikei@live.cn>
2021-09-30 09:02:45 +1000

    adding index for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    
+    def show
+        @user = User.find(params[:id])
+        @properties = @user.properties
+    end
+
+    def index
+        @users = User.where(status:"seller")
+    end
 
     def create
         @user = User.new(user_params)
@@ -13,7 +22,20 @@ class UsersController < ApplicationController
         else
           render 'new'
         end
-      end
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to properties_path
+            else
+            render 'edit'
+        end
+    end
     
     private
     def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     before_save{ self.email = self.email.downcase }
     validates :username, presence: true, 
                         uniqueness: {case_sensitive: false}, 
-                        length: { minimum: 3, maximum: 25 }
+                        length: { minimum: 3, maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                   
     validates :email, presence: true, 
                         uniqueness: {case_sensitive: false}, 
